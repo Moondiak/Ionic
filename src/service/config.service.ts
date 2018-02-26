24c040707d6b8fb86f10ed1dd7dcc4cb321cafb4
Jean-Marc AUBERT <jmaubert1333@gmail.com>
2018-02-26 08:19:33 +0100

    correction de config.service.ts

--- src/service/config.service.ts
+++ src/service/config.service.ts
@@ -2,7 +2,7 @@ import {Injectable} from '@angular/core';
 
 @Injectable()
 export class ConfigService {
-  private apiUrl = 'http://kim.jcatania.io:3000/pizza';
+  private apiUrl = 'http://kim.jcatania.io:3000';
 
   constructor() {
   }
